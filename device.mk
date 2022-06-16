58264bb66c1610a38526034f26ca85b9f4145170
daniml3 <daniel@danielml.dev>
2022-06-15 23:12:45 -0300

    veux: Show a ring on the camera cutout when its in use
    - For avoiding halo effect on low light conditions
    
    - Achieve this by creating a custom ImageView that its shown when the
      camera is in use

--- device.mk
+++ device.mk
@@ -112,6 +112,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service_64 \
     CameraGo \
     privapp-permissions-camera-go.xml \
+    CutoutRingServiceVeux \
     libcamera2ndk_vendor \
     libstdc++.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
